6ea133fa282c9305c2f9110d877253a66b179314
André Veríssimo <afsverissimo@gmail.com>
2016-06-03 16:33:11 +0100

    moves all functionality to ruby class

--- variations.rb
+++ variations.rb
@@ -1,48 +1,65 @@
 require 'bio'
 require 'byebug'
 
-def build_rev_trans_el(el, prefix = [])
-  table = Bio::CodonTable[1]
-
-  nt_seq = table.revtrans(el)
-
-  prefix << nt_seq
-  prefix
-end
-
-def build_rev_trans(seq)
-  prefix = []
-  seq.each_char do |el|
-    prefix = build_rev_trans_el(el, prefix)
-  end
-  prefix
-end
-
-# recursivo sem grandes requisitos de memoria
-def build_seq(f, res, name, id, prefix = '')
-  if res.nil? || res.empty?
-    f.write Bio::Sequence.auto(prefix).output_fasta("#{name}_#{id}")
-    return id + 1
-  end
+#
+class AA2NT
   #
-  combin = res.shift
-  combin.each do |el|
-    id = build_seq(f, res, name, id, prefix + el)
+  def initialize(table_num = 1)
+    @table = Bio::CodonTable[table_num]
   end
-  id
+
+  def revtrans(input_file = 'input.query', output_file = 'output.query')
+    fasta = nil
+    begin
+      File.open(input_file, 'r') do |f|
+        fasta = Bio::Alignment::MultiFastaFormat.new(f.read)
+      end
+    rescue
+      puts 'Error: Please check that \'input.query\' file exists' \
+        ' and in the Fasta format.'
+    end
+
+    File.open(output_file, 'wb') do |f|
+      fasta.entries.each_with_index do |entry, ix|
+        seq = Bio::Sequence::AA.new(entry.seq)
+        res = build_rev_trans(seq)
+        puts "#{ix + 1}/#{fasta.entries.size}: Writing all sequences of " \
+          "#{entry.entry_id}"
+        count = build_seq(f, res, entry.entry_id, 1)
+        puts "  #{count} reverse translation written"
+      end
+    end
+  end
+
+  def build_rev_trans_el(el, prefix = [])
+    nt_seq = @table.revtrans(el)
+
+    prefix << nt_seq
+    prefix
+  end
+
+  def build_rev_trans(seq)
+    prefix = []
+    seq.each_char do |el|
+      prefix = build_rev_trans_el(el, prefix)
+    end
+    prefix
+  end
+
+  # recursivo sem grandes requisitos de memoria
+  def build_seq(f, res, name, id, prefix = '')
+    if res.nil? || res.empty?
+      f.write Bio::Sequence.auto(prefix).output_fasta("#{name}_#{id}")
+      return id + 1
+    end
+    #
+    combin = res.shift
+    combin.each do |el|
+      id = build_seq(f, res, name, id, prefix + el)
+    end
+    id
+  end
 end
 
-fasta = nil
-File.open('input.query', 'r') do |f|
-  fasta = Bio::Alignment::MultiFastaFormat.new(f.read)
-end
-
-File.open('output.query', 'wb') do |f|
-  fasta.entries.each_with_index do |entry, ix|
-    seq = Bio::Sequence::AA.new(entry.seq)
-    res = build_rev_trans(seq)
-    puts "#{ix+1}/#{fasta.entries.size}: Writing all sequences of #{entry.entry_id}"
-    count = build_seq(f, res, entry.entry_id, 1)
-    puts "  #{count} reverse translation written"
-  end
-end
+myclass = AA2NT.new(1)
+myclass.revtrans
